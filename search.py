f64b1bcacd05317c06a1c225b399cee709f70f2a
lucas <hooftlucas@gmail.com>
2022-11-02 17:23:29 +0100

    A*search

--- search.py
+++ search.py
@@ -19,6 +19,7 @@ Pacman agents (in searchAgents.py).
 
 import util
 
+
 class SearchProblem:
     """
     This class outlines the structure of a search problem, but doesn't implement
@@ -71,7 +72,8 @@ def tinyMazeSearch(problem):
     from game import Directions
     s = Directions.SOUTH
     w = Directions.WEST
-    return  [s, s, w, s, w, w, s, w]
+    return [s, s, w, s, w, w, s, w]
+
 
 def depthFirstSearch(problem):
     """
@@ -101,11 +103,11 @@ def depthFirstSearch(problem):
     # ((5, 4), 'South', 1),
     solved: bool = False
     visitedNodes: list = []
-    pathMap = {} # For each state, store the parent
+    pathMap = {}  # For each state, store the parent
     solvedPath = []
 
-    currentState = problem.getStartState() # Get The Starting state
-    queue = util.Stack() # Define Queue
+    currentState = problem.getStartState()  # Get The Starting state
+    queue = util.Stack()  # Define Queue
     queue.push(currentState)
 
     while not solved:
@@ -129,80 +131,78 @@ def depthFirstSearch(problem):
 
         # 3) Explore nodes, push back to queue
         else:
-            allNeighbours = problem.getSuccesorStates(currentState) # Get Succesors
-            for state in allNeighbours: # Loop and push
-                if state not in visitedStates: # Prevents visiting nodes that already have been visited
+            allNeighbours = problem.getSuccesorStates(currentState)  # Get Succesors
+            for state in allNeighbours:  # Loop and push
+                if state not in visitedStates:  # Prevents visiting nodes that already have been visited
                     queue.push(state)
                     parentMap[state] = currentState
 
-
-
     # Return List Initialization
     return solvedPath
+
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
     goal: list = []
     goalReached = False
-    stateQueue = util.Queue() #we geven de state queue de maze ((x,y), dir, cost)
-    alreadyVisited = {} # we zetten de bezochte posities hierin
-    mazePath = {} #kijkt naar hoe routes gelinkt zijn aan de positie
+    stateQueue = util.Queue()  # we geven de state queue de maze ((x,y), dir, cost)
+    alreadyVisited = {}  # we zetten de bezochte posities hierin
+    mazePath = {}  # kijkt naar hoe routes gelinkt zijn aan de positie
     nextstep = 0
     stateQueue.push(problem.getStartState(), '')
 
     while not goalReached:
         xy, direction = stateQueue.pop()
-        alreadyVisited[xy] = direction #xy krijgt de directie die genomen is
-        if problem.isGoalState(xy): #controle of het doel bereikt is
+        alreadyVisited[xy] = direction  # xy krijgt de directie die genomen is
+        if problem.isGoalState(xy):  # controle of het doel bereikt is
             nextstep = xy
             goalReached = True
-            break #go out the if
-        for i in problem.getSuccessors(xy): # voor elke successor van xy, dus de huidige positie
-            #gaan elke successor af die nog niet bezocht is en nog niet in de queue staat
+            break  # go out the if
+        for i in problem.getSuccessors(xy):  # voor elke successor van xy, dus de huidige positie
+            # gaan elke successor af die nog niet bezocht is en nog niet in de queue staat
             if i[0] not in alreadyVisited.keys() and i[0] not in (j[0] for j in stateQueue.list):
-                mazePath[i[0]] = xy #gaan het pad van de huidige positie naar de succesor zetten
-                stateQueue.push((i[0], i[1])) # voeg de successor aan de stack
+                mazePath[i[0]] = xy  # gaan het pad van de huidige positie naar de succesor zetten
+                stateQueue.push((i[0], i[1]))  # voeg de successor aan de stack
 
-    while nextstep in mazePath.keys(): # follow the path
+    while nextstep in mazePath.keys():  # follow the path
         goal.append(alreadyVisited[nextstep])
-        currentStep = mazePath[nextstep] #set a new currentstep
-        nextstep = currentStep #nodig om deze lijst te reversen voor een juiste lijst te krijgen
+        currentStep = mazePath[nextstep]  # set a new currentstep
+        nextstep = currentStep  # nodig om deze lijst te reversen voor een juiste lijst te krijgen
     goal.reverse()
     return goal
 
+
 def uniformCostSearch(problem):
-    """Search the node of least total cost first."""
+    """Search the node of the least total cost first."""
     "*** YOUR CODE HERE ***"
-    goal: list = [] #list
+    goal: list = []  # list
     goalReached = False
-    StatePQ = util.PriorityQueue() #queue heeft de maze ((x,y), direction, cost), priority)
-    alreadyVisited = {} #kijken welke (xy) al bezocht zijn
-    mazepath = {} #kijken welke route gelinkt is aan de positie
+    StatePQ = util.PriorityQueue()  # queue heeft de maze ((x,y), direction, cost), priority)
+    alreadyVisited = {}  # kijken welke (xy) al bezocht zijn
+    mazepath = {}  # kijken welke route gelinkt is aan de positie
     mazecost = {}
     nextstep = 0
-    StatePQ.push((problem.getStartState(),'',0),0)
+    StatePQ.push((problem.getStartState(), '', 0), 0)
     while not goalReached:
-        xy, direction = stateQueue.pop()
-        alreadyVisited[xy] = direction # set the direction where he was in the list
-        if problem.isGoalState(xy): #control if the destination is the goal
+        xy, dir, cost = StatePQ.pop()
+        alreadyVisited[xy] = dir  # set the direction where he was in the list
+        if problem.isGoalState(xy):  # control if the destination is the goal
             nextstep = xy
-            goalReached = True
-            break #go out the if
-        for i in problem.getSuccessors(xy): #elke successor van xy -> huidige positie
-            if i[0] not in alreadyVisited.keys(): #kijken of het niet eerder bezocht is
-                totalCost = cost +i[2]
+            break  # go out the if
+        for i in problem.getSuccessors(xy):  # elke successor van xy -> huidige positie
+            if i[0] not in alreadyVisited.keys():  # kijken of het niet eerder bezocht is
+                totalCost = cost + i[2]
                 mazecost[i[0]] = totalCost
                 mazepath[i[0]] = xy
-                stateQueue.push((i[0], i[1], totalCost), totalcost)# push de xy, dir, totalcost op de stack
+                StatePQ.push((i[0], i[1], totalCost), totalCost)  # push de xy, dir, totalcost op de stack
 
-    while nextstep in mazePath.keys(): # follow the path
+    while nextstep in mazepath.keys():  # follow the path
         goal.append(alreadyVisited[nextstep])
-        currentStep = mazePath[nextstep] #set a new currentstep
+        currentStep = mazepath[nextstep]  # set a new currentstep
         nextstep = currentStep
     goal.reverse()
     return goal
-
 
 
 def nullHeuristic(state, problem=None):
@@ -212,10 +212,43 @@ def nullHeuristic(state, problem=None):
     """
     return 0
 
+
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    goal: list = []
+    goalReached = False
+    StatePQ = util.PriorityQueue() #queue has maze((x,y), dir, cost)
+    alreadyVisited = {} #it knows the visited route
+    mazePath = {}
+    mazeCost = {}
+    nextStep = 0
+
+    StatePQ.push((problem.getStartState(), ' ', 0), 0)
+
+    while not goalReached:
+        xy, dir, cost = StatePQ.pop()
+        alreadyVisited[xy] = dir #xy get the next direction
+        if problem.isGoalState(xy): #this position is the goal
+            nextStep = xy #this is the position this moment
+            goalReached = True
+            break
+        for i in problem.getSuccessors(xy): #for every succesor xy
+            if i[0] not in alreadyVisited.keys(): #this position is not visited: i[0] is (x,y) of succesor
+                totalcost = cost +i[2] #totalcost is the sum of the all costes
+                heuristicCost = totalcost + heuristic(i[0], problem) # heuristic + totalcost by A*
+                if not (i[0] in mazeCost.keys() and mazeCost[i[0]] <= totalcost):
+                    mazeCost[i[0]] = totalcost #cost to get to the goalpoint
+                    mazePath[i[0]] = xy # moment point to get to the succesor
+                    StatePQ.push((i[0],i[1],totalcost), heuristicCost) # push xy, dir and totalcost on the stack
+    while nextStep in mazePath.keys(): #follow the path
+        goal.append(alreadyVisited[nextStep]) #put the action (NWSE) to get the goal list
+        currentStep = mazePath[nextStep] #get currstep out of the succesor
+        nextStep = currentStep
+    goal.reverse() #reverse to get the right sequence
+    return goal
+
+
 
 
 # Abbreviations
